d73742b27f21e6ab128e5891f318e30931e4d34b
emmanuel <entibimenya03@gmail.com>
2025-02-13 11:59:44 +0000

    styling the app using inline and external css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
 const pizzaData = [
   {
     name: "Focaccia",
@@ -47,25 +48,71 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
+  );
+}
+function Header() {
+  const style = { color: "red", fontSize: "48", textTransform: "upperCase" };
+  return (
+    //styling using inline stylinin //first use style= then enter the
+    //  javascript mode by {} then another {} for creating and object //font-size
+    // is not allowed in javascript mode instead use camel case fontSize // we
+    //  can also create a variable name and pass in the properties
+    <header className="header">
+      <h1
+      //style={{ color: "red", fontSize: "48", textTransform: "upperCase" }}
+      // style={style}
+      >
+        Fast React Pizza Co.
+      </h1>
+    </header>
+  );
+}
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  //   if (hour >= openHour && hour <= closeHour) alert("W're currently open!");
+  //   else alert("Sorry w're closed!");
+  console.log(isOpen);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We are currently open
+    </footer>
   );
 }
 function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
 }
 //React v18
 const root = ReactDom.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
